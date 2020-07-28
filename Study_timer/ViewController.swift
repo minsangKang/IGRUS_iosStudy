534df421319fe2f8caaf96720d7b3744ebd51b2a
Min_MacbookPro <fdee@Min-MacbookProui-MacBookPro.local>
2020-07-28 15:43:41 +0900

    7/28 2주차 프로젝트 진행 내용

--- Study_timer/ViewController.swift
+++ Study_timer/ViewController.swift
@@ -10,27 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var Label_hello: UILabel!
-    @IBOutlet weak var Text_num: UITextField!
-    @IBOutlet weak var Text_name: UITextField!
+    @IBOutlet weak var Label_Persent: UILabel!
+    @IBOutlet weak var Label_AllTime: UILabel!
+    @IBOutlet weak var Label_SumTime: UILabel!
+    @IBOutlet weak var Label_Timer: UILabel!
+    
+    @IBOutlet weak var Button_START_Outlet: UIButton!
+    @IBOutlet weak var Button_STOP_Outlet: UIButton!
+    @IBOutlet weak var Button_RESTART_Outlet: UIButton!
     
     
+    //화면에 들어왔을 시 실행되는 메소드
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Button_START_Outlet.layer.cornerRadius = 8
+        Button_STOP_Outlet.layer.cornerRadius = 8
+        Button_RESTART_Outlet.layer.cornerRadius = 8
     }
 
-    @IBAction func Button_click(_ sender: UIButton) {
-//        Label_hello.text = "Button Click!"
-        
-        //버튼을 클릭 했을 시 학번, 이름을 보여준다
-//        Label_hello.text = "학번 : " + Text_num.text! + "\n이름 : " + Text_name.text!
-        
-        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "View2") else {return}
-        
-        self.present(nextVC, animated: true)
-        
+    @IBAction func Button_START_Action(_ sender: UIButton) {
+        Label_Timer.text = "안녕하세요!"
     }
+    @IBAction func Button_STOP_Action(_ sender: UIButton) {
+        Label_Timer.text = "종료!"
+    }
+    @IBAction func Button_RESTART_Action(_ sender: UIButton) {
+        Label_Timer.text = "재시작!"
+    }
+    @IBAction func Button_RESET_Action(_ sender: UIButton) {
+        Label_Timer.text = "초기화!"
+    }
+    @IBAction func Button_TimeSET_Action(_ sender: UIButton) {
+        Label_Timer.text = "시간 설정!"
+    }
+    
     
 }
 
