ba7c63281c21a9c7aa1d1815ce4a1acce87bb30d
FreeDeveloper97 <freedeveloper97@gmail.com>
2020-08-11 12:59:11 +0900

    8/11 4주차 진행상황

--- Study_timer/ViewController.swift
+++ Study_timer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
@@ -18,6 +19,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button_START_Outlet: UIButton!
     @IBOutlet weak var Button_STOP_Outlet: UIButton!
     @IBOutlet weak var Button_RESTART_Outlet: UIButton!
+    
+    @IBOutlet weak var Button_RESET_outlet: UIButton!
+    @IBOutlet weak var Button_Time_SET: UIButton!
     
     //타이머 시간 : 50분으로 설정
     //총 남은시간 : 8시간 설정
@@ -35,6 +39,12 @@ class ViewController: UIViewController {
     //타이머 작동을 위한 기본작업?
     var realTime = Timer()
     
+    let BUTTON_CLICK = UIColor(named: "ButtonClick")
+    let BUTTON_COLOR = UIColor(named: "ButtonColor")
+    let BACKGROUND_COLOR = UIColor(named: "BackgroundColor")
+    
+    
+    
     //화면에 들어왔을 시 실행되는 메소드
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,24 +55,39 @@ class ViewController: UIViewController {
     }
 
     @IBAction func Button_START_Action(_ sender: UIButton) {
-        Label_Timer.text = "안녕하세요!"
         //버튼 클릭했을 때 타이머 작동할수 있는 상태일 경우 -> 타이머 작동
         if(timeTrigger)
         {
             checkTimeTrigger()
             timeTrigger = false
         }
+        //실행시 배경색을 검은색으로 바꾸겠다
+        startColor()
+        //시작을 누른 경우 시작, 재시작 버튼 비활성화, stop 버튼 활성
+        startEnable()
     }
     @IBAction func Button_STOP_Action(_ sender: UIButton) {
-        Label_Timer.text = "종료!"
-        realTime.invalidate()
-        timeTrigger = true
+        endGame()
+        //스탑된 경우 배경색을 바꾸겠다
+        stopColor()
+        //스탑이 된 경우 START 버튼을 클릭할 수 있게 설정하겠다
+        stopEnable()
     }
     @IBAction func Button_RESTART_Action(_ sender: UIButton) {
-        Label_Timer.text = "재시작!"
+        Int_seconds = 3000
+        updateShow()
+        stopColor()
+        stopEnable()
+        Button_RESTART_Outlet.backgroundColor = BUTTON_CLICK
+        Button_RESTART_Outlet.isUserInteractionEnabled = false
     }
     @IBAction func Button_RESET_Action(_ sender: UIButton) {
-        Label_Timer.text = "초기화!"
+        getTimeData()
+        updateShow()
+        stopColor()
+        stopEnable()
+        Button_RESTART_Outlet.backgroundColor = BUTTON_CLICK
+        Button_RESTART_Outlet.isUserInteractionEnabled = false
     }
     @IBAction func Button_TimeSET_Action(_ sender: UIButton) {
         Label_Timer.text = "시간 설정!"
@@ -74,12 +99,29 @@ class ViewController: UIViewController {
     }
     
     @objc func updateCounter(){
-        Int_seconds = Int_seconds - 1
-        Int_sum = Int_sum + 1
-        Int_allTimes = Int_allTimes - 1
-        print("타이머 시간 : " + String(Int_seconds))
-        print("누적 시간 : " + String(Int_sum))
-        print("총 남은 시간 : " + String(Int_allTimes))
+        //종료될 경우 생성
+        
+        //타이머 시간이 종료된 경우
+        if(Int_seconds < 1)
+        {
+            endGame()
+            stopColor()
+            stopEnable()
+            AudioServicesPlaySystemSound(1254) //진동
+            AudioServicesPlaySystemSound(4095) //특정한 소리
+        }
+        //총 남은시간이 종료된 경우
+        else if(Int_allTimes < 1)
+        {
+            endGame()
+        }
+        //그외에 타이머가 실행되어야 하는 경우
+        else
+        {
+            Int_seconds = Int_seconds - 1
+            Int_sum = Int_sum + 1
+            Int_allTimes = Int_allTimes - 1
+        }
         updateShow()
     }
     
@@ -106,8 +148,61 @@ class ViewController: UIViewController {
         let H: Int = temp/3600
         let M: Int = temp/60 - H*60
         
-        let returnString = String(H) + ":" + String(M) + ":" + String(S)
+        //삼항연산자를 이용하여 만약에 10보다 작은 경우 0을 추가하여 출력하겠다
+        let stringS = S<10 ? "0"+String(S) : String(S)
+        let stringM = M<10 ? "0"+String(M) : String(M)
+        
+        let returnString = String(H) + ":" + stringM + ":" + stringS
         return returnString
+    }
+    
+    //정지되는 경우의 메소드
+    func endGame()
+    {
+        realTime.invalidate()
+        timeTrigger = true
+    }
+    
+    func startColor()
+    {
+        self.view.backgroundColor = UIColor.black
+        Button_START_Outlet.backgroundColor = BUTTON_CLICK
+        Button_STOP_Outlet.backgroundColor = BUTTON_COLOR
+        Button_RESTART_Outlet.backgroundColor = BUTTON_CLICK
+    }
+    
+    func startEnable()
+    {
+        Button_START_Outlet.isUserInteractionEnabled = false
+        Button_STOP_Outlet.isUserInteractionEnabled = true
+        Button_RESTART_Outlet.isUserInteractionEnabled = false
+        Button_RESET_outlet.isUserInteractionEnabled = false
+        Button_Time_SET.isUserInteractionEnabled = false
+    }
+    
+    func stopColor()
+    {
+        self.view.backgroundColor = BACKGROUND_COLOR
+        Button_START_Outlet.backgroundColor = BUTTON_COLOR
+        Button_STOP_Outlet.backgroundColor = BUTTON_CLICK
+        Button_RESTART_Outlet.backgroundColor = BUTTON_COLOR
+    }
+    
+    func stopEnable()
+    {
+        Button_START_Outlet.isUserInteractionEnabled = true
+        Button_STOP_Outlet.isUserInteractionEnabled = false
+        Button_RESTART_Outlet.isUserInteractionEnabled = true
+        Button_RESET_outlet.isUserInteractionEnabled = true
+        Button_Time_SET.isUserInteractionEnabled = true
+    }
+    
+    //저장되어 있는 시간을 불러오는 메소드, 8시간, 50분을 재설정 해주는 메소드
+    func getTimeData()
+    {
+        Int_seconds = 3000
+        Int_allTimes = 28800
+        Int_sum = 0
     }
     
 }
